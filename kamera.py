7a502beca69e158fb24d02b69c566ec769195b44
halco102 <ado.halilovic@outlook.com>
2021-09-23 17:07:24 +0200

    deleting imports and comments

--- kamera.py
+++ kamera.py
@@ -1,32 +1,22 @@
-from turtle import goto
-
 import cv2
 import numpy as np
-import re
 import pytesseract
 import imutils
 
 
 def test(img):
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # convert to grey scale
-    gray = cv2.bilateralFilter(gray, 11, 17, 17)  # Blur to reduce noise
-    edged = cv2.Canny(gray, 30, 200)  # Perform Edge detection
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = cv2.bilateralFilter(gray, 11, 17, 17)
+    edged = cv2.Canny(gray, 30, 200)
 
-    # find contours in the edged image, keep only the largest
-    # ones, and initialize our screen contour
     cnts = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
     cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[:10]
     screenCnt = None
 
-    # loop over our contours
     for c in cnts:
-        # approximate the contour
         peri = cv2.arcLength(c, True)
         approx = cv2.approxPolyDP(c, 0.018 * peri, True)
-
-        # if our approximated contour has four points, then
-        # we can assume that we have found our screen
         if len(approx) == 4:
             screenCnt = approx
             break
@@ -41,12 +31,10 @@ def test(img):
     if detected == 1:
         cv2.drawContours(img, [screenCnt], -1, (0, 255, 0), 3)
 
-    # Masking the part other than the number plate
     mask = np.zeros(gray.shape, np.uint8)
     new_image = cv2.drawContours(mask, [screenCnt], 0, 255, -1, )
     new_image = cv2.bitwise_and(img, img, mask=mask)
 
-    # Now crop
     (x, y) = np.where(mask == 255)
     (topx, topy) = (np.min(x), np.min(y))
     (bottomx, bottomy) = (np.max(x), np.max(y))
@@ -55,13 +43,6 @@ def test(img):
     # Read the number plate
     text = pytesseract.image_to_string(Cropped, config='--psm 11')
     print("Detected Number is:", text)
-
-    #cv2.imshow('image', img)
-    #cv2.imshow('Cropped', Cropped)
-    #img.release()
-    #cv2.destroyAllWindows()
-
-
 
 cam = cv2.VideoCapture(0)
 
